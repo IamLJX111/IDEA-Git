b143efd54aac59b38d5deca95c6e8364f43b6af6
LJX <3065852331@qq.com>
2024-10-10 16:16:07 +0800

    hot-fix first commit

--- src/P1/Demo.java
+++ src/P1/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("第一版本");
         System.out.println("第二版本");
+        System.out.println("要冲突合并了!");
     }
 }
