89638828cd8c1fc6604f09ed98534d4e7713b58c
LJX <3065852331@qq.com>
2024-10-10 16:16:56 +0800

    master third commit

--- src/P1/Demo.java
+++ src/P1/Demo.java
@@ -4,5 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("第一版本");
         System.out.println("第二版本");
+        System.out.println("hot-fix分支的代码修改了,合并前我master分支的代码也修改，所以此时再合并会冲突!");
     }
 }
